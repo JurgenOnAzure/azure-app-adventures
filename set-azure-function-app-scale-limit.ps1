ccbf98a534025ed28368bff66fe35ceb52cf9651
Jurgen Kevelaers <62745957+JurgenOnAzure@users.noreply.github.com>
2021-11-16 19:39:13 +0100

    Add files via upload
    Script that goes along with this blog post: https://jurgenonazure.com/2021/11/azure-function-app-adventures-setting-the-scale-out-limit-with-powershell

---
+++ set-azure-function-app-scale-limit.ps1
@@ -0,0 +1,36 @@
+﻿# This script shows you how to set the 'App Scale Out' limit on an Azure Function App,
+# as described in this blog post: https://jurgenonazure.com/2021/11/azure-function-app-adventures-setting-the-scale-out-limit-with-powershell
+
+
+# NOTE: if you haven't installed the Az PowerShell module yet,
+# look here: https://docs.microsoft.com/ko-kr/powershell/azure/install-az-ps
+
+
+### INPUT ###
+
+$subscriptionId="<TODO: set your Azure subscription ID here>"
+$resourceGroupName = "app-blog-rg"
+$functionAppName = "app-blog-func-app"
+$functionAppScaleLimit=10
+
+
+### LOGIC ###
+
+# Login to Azure
+Connect-AzAccount -SubscriptionId $subscriptionId
+
+Write-Output ""
+Write-Output "SETTING THE SCALE LIMIT on $($functionAppName)"
+
+# Get function app with properties
+$resource = Get-AzResource `
+    -ResourceType Microsoft.Web/sites `
+    -ResourceGroupName $resourceGroupName `
+    -ResourceName "$($functionAppName)/config/web"
+
+# Set scale limit on properties and apply
+$resource.Properties.functionAppScaleLimit = $functionAppScaleLimit
+$resource | Set-AzResource -Force
+
+Write-Output ""
+Write-Output "DONE!"
